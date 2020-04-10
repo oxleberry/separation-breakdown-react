022296e31e11a4da43e4d6dae627da5255c8eaec
Sharon Paz <oxleberry@gmail.com>
2020-04-10 13:24:00 -0700

    Adding html and css from orig separation breakdown proj.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> PRINTED ONESIE </h2>
+      <div className="wrap">
+        <div className="screenprint">
+          <div className="teeGroup">
+            <div className="tBefore">
+            <p>Design from the De Young Museum. <br />
+            Color Separations by Oxleberry.</p>
+            </div>
+            <div className="tAfter"><div className="t0"></div></div>
+          </div>
+        </div>
+      </div>
+
+      <h2> SCREENPRINT BREAKDOWN</h2>
+      <div className="wrap">
+        <div className="screenprint">
+          <div className="inkGroup">
+              <button className="inkText ink i1" id="p1"> ub </button>
+              <button className="inkText ink i2" id="p2"> 7532 </button>
+              <button className="inkText ink i3" id="p3"> 468 </button>
+              <button className="inkText ink i4" id="p4"> 174 </button>
+              <button className="inkText ink i5" id="p5"> wht </button>
+              <button className="ink reset" id="pReset"> reset </button>	
+          </div>
+          <div className="printGroup">
+            <div className="pBefore">
+              <div className="print order_p1"></div>
+              <div className="print order_p2"></div>
+              <div className="print order_p3"></div>
+              <div className="print order_p4"></div>
+              <div className="print order_p5"></div>
+              <div className="print pAll"></div>											
+            </div>
+            <div className="pAfter">
+              <div className="print"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
