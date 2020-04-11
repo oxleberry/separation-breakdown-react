aca69f3bc08f74afdcfdfcbff4361717b8af11af
Sharon Paz <oxleberry@gmail.com>
2020-04-11 08:03:25 -0700

    Mapping through InkButtonList.

--- src/components/InkButton.js
+++ src/components/InkButton.js
@@ -1,19 +1,59 @@
-
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class InkButton extends Component {
-
-  clickHandler() {
-    console.log('click');
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPressed: false
+    }
+    this.clickHandler = this.clickHandler.bind(this)
   }
 
-  render() {
+  clickHandler() {
+    let pressed = this.state.isPressed;
+    this.setState({isPressed: !pressed})
+  }
+
+  buttonPressed() {
+    return (
+      <button className={`${this.props.labelColor} ink ${this.props.color} active`} id={this.props.id} onClick={this.clickHandler}> 
+        {this.props.name} 
+      </button>
+    )
+  }
+
+  buttonDisplayed() {
     return (
       <button className={`${this.props.labelColor} ink ${this.props.color}`} id={this.props.id} onClick={this.clickHandler}> 
         {this.props.name} 
       </button>
     )
   }
+
+  render() {
+    if (this.state.isPressed) {
+      return this.buttonPressed();
+    } else {
+      return this.buttonDisplayed();
+    }
+  }
+}
+
+// Type Checking
+InkButton.propTypes = { 
+  name:PropTypes.string,
+  color:PropTypes.string,
+  labelColor:PropTypes.string,
+  id:PropTypes.string
+}
+
+// Set Default Props
+InkButton.defaultProps = {
+  name: 'empty',
+  color: 'reset',
+  labelColor: 'inkText',
+  id: ''
 }
 
 export default InkButton;
--- src/components/InkButtonList.js
+++ src/components/InkButtonList.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import InkButton from '../components/InkButton';
 
+let initInksData = [
+  {name: 'ub', color: 'i1', labelColor: "inkText", id: "p1"},
+  {name: '7532', color: 'i2', labelColor: "inkText", id: "p2"},
+  {name: '468', color: 'i3', labelColor: "inkText", id: "p3"},
+  {name: '174', color: 'i4', labelColor: "inkText", id: "p4"},
+  {name: 'white', color: 'i5', labelColor: "inkText", id: "p5"},
+  {name: 'reset', color: 'reset', labelColor: "", id: "pReset"}
+]
+
 class InkButtonList extends Component {
+  eachInkButton(data, i) {
+    return (
+      <InkButton 
+        key={i} 
+        name={data.name} 
+        color={data.color} 
+        labelColor={data.labelColor} 
+        id={data.id}>
+      </InkButton>
+    )
+  }
 
   render() {
-
+    // Iterating from the init Data.
+    let eachInkButton = initInksData.map(this.eachInkButton)
     return (
       <div className="inkGroup">
-        {/* TODO: refactor "inkText" to labelColor = darkText, or lightText */}
-        <InkButton name="ub" color="i1" labelColor="inkText" id="p1" />
-        <InkButton name="7532" color="i2" labelColor="inkText" id="p2" />
-        <InkButton name="468" color="i3" labelColor="inkText" id="p3" />
-        <InkButton name="174" color="i4" labelColor="inkText" id="p4" />
-        <InkButton name="wht" color="i5" labelColor="inkText" id="p5" />
-        <InkButton name="reset" color="reset" labelColor="" id="pReset" />
+        {eachInkButton}
       </div>
     )
   }
